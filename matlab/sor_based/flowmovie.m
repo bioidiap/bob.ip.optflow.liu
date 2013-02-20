8d402a69939450a75a96c78a248ff720d59a7562
Andre Anjos <andre.anjos@idiap.ch>
2013-02-20 18:10:07 +0100

    Improve based matlab functionality (SOR)

--- matlab/sor_based/flowmovie.m
+++ matlab/sor_based/flowmovie.m
@@ -1,22 +1,74 @@
-function [] = flowmovie(inFile, outDir)
+function [] = flowmovie(inFile, outDir, grayScale, maxFrames)
 
-% flowmovie(inFile, outDir)
+% flowmovie(inFile, outDir, grayScale, maxFrames)
 %
 % Computes the flow between every two successive frames of the given input
 % movie file and stores the computed flow as .hdf5 to the output directory.
 %
+% If the parameter `grayScale` is set to 0 (the default), then the input data
+% is fed as it is into the flow calculation procedure. Else, it is gray-scaled
+% just before using `rgb2gray()`.
+%
+% The parameter `maxFrames` controls the maximum number of frames that will
+% have their optical flow calculated for. By default, use all available frames.
+%
 % This function is based on the original work from Murali M. Chakka and leads
-% to *exact* the same results as his hold implementation.
+% to *exact* the same results as his old implementation.
 %
 % Improved by Andre Anjos <andre.anjos@idiap.ch>
-% Thu 15 Nov 2012 16:55:36 CET
+% Thu 15 Nov 2012 16:55:36 CET -- initial version
+% Wed 20 Feb 2013 15:58:22 CET -- optional pre-gray-scaling and max frames
+
+if nargin == 0
+  disp('usage: flowmovie <input-file> <output-dir> <pre-grayscale> <frames>');
+  return;
+end
+
+if nargin < 3
+  grayScale = 0;
+else
+  grayScale = str2num(grayScale);
+end
 
 addpath('mex');
 
 disp(sprintf('Loading contents of %s...', inFile));
 videoObject = mmreader(inFile);
 frames = read(videoObject) ; % frames will be uint8 at this point
-disp(sprintf('Loaded %d frames.', videoObject.NumberOfFrames));
+N = videoObject.NumberOfFrames;
+isColor = 1; % movie files are always colored
+
+% Use this to load HDF5 files produced by Bob instead
+%frames = hdf5read(inFile, 'array');
+%isColor = size(size(frames),2) == 4;
+%isColor = size(size(frames),2) == 4;
+%if isColor == 1
+%  frames = permute(frames, [2 1 3 4]);
+%  N = size(frames,4);
+%else
+%  frames = permute(frames, [2 1 3]);
+%  N = size(frames,3);
+%end
+
+disp(sprintf('Loaded %d frames.', N));
+
+if nargin < 4
+  N = size(frames,4);
+else
+  N = str2num(maxFrames);
+end
+
+disp(sprintf('Running flow analysis for %d frames:', N));
+
+if grayScale ~= 0 && isColor == 1
+  % Convert frames to grayscale
+  for i = 1 : N
+    disp(sprintf(' * Converting frame #%d to grayscale', i));
+    grayFrames(:, :, i) = rgb2gray(frames(:, :, :, i));
+  end
+  frames = grayFrames;
+  isColor = 0;
+end
 
 % Set optical flow parameters (see Coarse2FineTwoFrames.m for the definition of
 % the parameters)
@@ -29,10 +81,15 @@ nIterations = 20;
 
 para = [alpha,ratio,minWidth,nOuterFPIterations,nInnerFPIterations,nIterations];
 
-for i = 1 : a.NumberOfFrames-1
+for i = 1 : N-1
   disp(sprintf(' * Evaluating flow between frames #%d -> #%d', i, i+1));
-  im1 = im2double(frames(:, :, :, i));
-  im2 = im2double(frames(:, :, :, i+1));
+  if isColor == 1
+    im1 = im2double(frames(:, :, :, i));
+    im2 = im2double(frames(:, :, :, i+1));
+  else
+    im1 = im2double(frames(:, :, i));
+    im2 = im2double(frames(:, :, i+1));
+  end
   [u v w2] = Coarse2FineTwoFrames(im1,im2,para);
   uv(:,:,1,i) = u';
   uv(:,:,2,i) = v';
@@ -48,7 +105,6 @@ if exist(outFile, 'file') ~= 0
 end
 h5create(outFile, '/uv', size(uv));
 h5write(outFile, '/uv', uv);
-h5create(outFile, '/frames', size(frames(:,:,:));
 h5writeatt(outFile, '/uv', 'method', 'SOR');
 h5writeatt(outFile, '/uv', 'alpha', alpha);
 h5writeatt(outFile, '/uv', 'ratio', ratio);
