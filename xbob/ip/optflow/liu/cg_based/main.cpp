ea8c34b211ea91b792c4fdd9424f601f58e1e57d
Andre Anjos <andre.dos.anjos@gmail.com>
2014-04-02 20:07:08 +0200

    Use pure Python C-API

--- xbob/ip/optflow/liu/cg_based/main.cpp
+++ xbob/ip/optflow/liu/cg_based/main.cpp
@@ -1,51 +1,40 @@
 /**
  * @author Andre Anjos <andre.anjos@idiap.ch>
- * @date Thu 08 Nov 2012 12:09:32 CET 
+ * @date Thu 08 Nov 2012 12:09:32 CET
  *
  * @brief Boost.Python extension to Ce Liu's Optical Flow dense estimator
  */
 
-#include <boost/python.hpp>
-
-#include <bob/config.h>
-#ifdef BOB_API_VERSION
-#  include <bob/python/gil.h>
-#  include <bob/python/ndarray.h>
-#else
-#  include <bob/core/python/gil.h>
-#  include <bob/core/python/ndarray.h>
+#ifdef NO_IMPORT_ARRAY
+#undef NO_IMPORT_ARRAY
 #endif
+#include <xbob.blitz/capi.h>
+#include <xbob.blitz/cleanup.h>
 
 #include "OpticalFlow.h"
-
-using namespace boost::python;
 
 /**
  * Temporarily assigns the memory storage from the blitz array to the double
  * image type that is used by Liu's framework.
  */
-static void bz2dimage(blitz::Array<double,2>& bz, cg::DImage& di) {
+static void bz2dimage(PyBlitzArrayObject* bz, cg::DImage& di) {
   di.clear();
-  //di.ConvertFromMatlab<double>(bz.data(), bz.extent(1), bz.extent(0), 1);
-  di.imWidth = bz.extent(1);
-  di.imHeight = bz.extent(0);
-  di.nChannels = 1;
-  di.computeDimension();
-  di.pData = bz.data();
+  if (bz->ndim == 2) {
+    di.imWidth = bz->shape[1];
+    di.imHeight = bz->shape[0];
+    di.nChannels = 1;
+    di.computeDimension();
+    di.pData = bz->data;
+  }
+  else {
+    di.ConvertFromMatlab<double>(bz->data, bz->shape[2],
+        bz->shape[1], bz->shape[0]);
+  }
 }
 
-/**
- * Copies and transposes 3D array data
- */
-static void bz2dimage(blitz::Array<double,3>& bz, cg::DImage& di) {
-  di.clear();
-  di.ConvertFromMatlab<double>(bz.data(), bz.extent(2), bz.extent(1),
-      bz.extent(0));
-}
-
-static tuple coarse2fine_flow (
-    bob::python::const_ndarray i1, //first input image
-    bob::python::const_ndarray i2, //second input image
+static PyObject* coarse2fine_flow (
+    PyBlitzArrayObject* i1, //first input image
+    PyBlitzArrayObject* i2, //second input image
     double alpha=0.02,
     double ratio=0.75,
     int minWidth=30,
@@ -54,43 +43,12 @@ static tuple coarse2fine_flow (
     int nCGIterations=50
     ) {
 
-  bob::core::array::typeinfo info = i1.type();
   cg::DImage di1;
   cg::DImage di2;
 
-  if (info.nd == 2) {
-
-    //Checks Im1 and Im2 are convertible into double/grayscaled images
-    blitz::Array<double,2> bz1 = i1.cast<double,2>();
-    blitz::Array<double,2> bz2 = i2.cast<double,2>();
-
-    if (bz1.extent(0) != bz2.extent(0) || 
-        bz1.extent(1) != bz2.extent(1)) {
-      throw std::runtime_error("shapes of input images differ");
-    }
-
-    //Maps input images
-    bz2dimage(bz1, di1);
-    bz2dimage(bz2, di2);
-
-  }
-  else {
-
-    //Checks Im1 and Im2 are convertible into double/grayscaled images
-    blitz::Array<double,3> bz1 = i1.cast<double,3>();
-    blitz::Array<double,3> bz2 = i2.cast<double,3>();
-
-    if (bz1.extent(0) != bz2.extent(0) || 
-        bz1.extent(1) != bz2.extent(1) ||
-        bz1.extent(2) != bz2.extent(2)) {
-      throw std::runtime_error("shapes of input images differ");
-    }
-
-    //Maps input images
-    bz2dimage(bz1, di1);
-    bz2dimage(bz2, di2);
-
-  }
+  //Maps input images
+  bz2dimage(i1, di1);
+  bz2dimage(i2, di2);
 
   //Output arrays
   cg::DImage du;
@@ -110,80 +68,222 @@ static tuple coarse2fine_flow (
   //else { for info.nd == 3 we do have to delete! }
 
   //Copies output data back
-  tuple retval;
-  
-  bob::python::py_array u(bob::core::array::t_float64, di1.imHeight, di1.imWidth);
-  memcpy(u.ptr(), du.pData, sizeof(double)*du.nElements);
-  bob::python::py_array v(bob::core::array::t_float64, di1.imHeight, di1.imWidth);
-  memcpy(v.ptr(), dv.pData, sizeof(double)*dv.nElements);
+  PyObject* u = PyArray_SimpleNew(2, i1->shape, NPY_FLOAT64);
+  if (!u) return 0;
+  auto u_ = make_safe(u);
+  void* u_data = PyArray_DATA(u);
+  memcpy(u_data, du.pData, sizeof(double)*du.nElements);
+
+  PyObject* v = PyArray_SimpleNew(2, i1->shape, NPY_FLOAT64);
+  if (!v) return 0;
+  auto v_ = make_safe(v);
+  void* v_data = PyArray_DATA(v);
+  memcpy(v_data, dv.pData, sizeof(double)*dv.nElements);
+
+  PyObject* w2 = PyArray_SimpleNew(2, i1->shape, NPY_FLOAT64);
+  if (!w2) return 0;
+  auto w2_ = make_safe(w2);
+  void* w2_data = PyArray_DATA(w2);
 
   if (info.nd == 2) {
-
-    bob::python::py_array w2(bob::core::array::t_float64, di1.imHeight, di1.imWidth);
-    memcpy(w2.ptr(), dwarped_i2.pData, sizeof(double)*dwarped_i2.nElements);
-    retval = make_tuple(u.pyobject(), v.pyobject(), w2.pyobject());
-
+    memcpy(w2_data, dwarped_i2.pData, sizeof(double)*dwarped_i2.nElements);
   }
-
   else {
-
-    bob::python::py_array w2(bob::core::array::t_float64, di1.nChannels, di1.imHeight, di1.imWidth);
-    dwarped_i2.ConvertToMatlab(static_cast<double*>(w2.ptr()));
-    retval = make_tuple(u.pyobject(), v.pyobject(), w2.pyobject());
-
+    dwarped_i2.ConvertToMatlab(static_cast<double*>(w2_data));
   }
 
-  return retval;
+  return Py_BuildValue("(OOO)", u, v, w2);
 }
 
-namespace cg {
-  BOOST_PYTHON_FUNCTION_OVERLOADS(coarse2fine_flow_overloads, coarse2fine_flow, 2, 8)
+PyDoc_STRVAR(s_flow_str, "flow");
+PyDoc_STRVAR(s_flow_doc,
+"flow(i1, i2, [alpha=0.02, [ratio=0.75, [min_width=30, [n_outer_fp_iterations=20, [n_inner_fp_iterations=1, [n_cg_iterations=50]]]]]]) -> (u, v, w2)\n\
+\n\
+This method computes the dense optical flow field using a\n\
+coarse-to-fine approach. C++ code running under this call is\n\
+extracted from **the old version (pre Aug 1, 2011)** of\n\
+`Ce Liu's homepage\n\
+<http://people.csail.mit.edu/celiu/OpticalFlow/>`_ and should\n\
+give the exact same output as the Matlab equivalent.\n\
+\n\
+.. note::\n\
+\n\
+   This variant does not use the Successive Over-Relaxation\n\
+   (SOR) that was implemented on August 1st., 2011 by C. Liu,\n\
+   but the old version based on Conjugate-Gradient (CG).\n\
+\n\
+Parameters:\n\
+\n\
+i1\n\
+  First input frame (grayscale/double image)\n\
+\n\
+i2\n\
+  Second input frame (same dimension and type of the first frame)\n\
+\n\
+alpha\n\
+  [optional] Regularization weight\n\
+\n\
+ratio\n\
+  [optional] Downsample ratio\n\
+\n\
+min_width\n\
+  [optional] Width of the coarsest level\n\
+\n\
+n_outer_fp_iterations\n\
+  [optional] The number of outer fixed point iterations\n\
+\n\
+n_inner_fp_iterations\n\
+  [optional] The number of inner fixed point iterations\n\
+\n\
+n_cg_iterations\n\
+  [optional] The number of conjugate-gradient (CG) iterations\n\
+\n\
+Returns a tuple containing three 2D double arrays with the same\n\
+dimensions as the input images:\n\
+\n\
+u\n\
+  Output velocities in ``x`` (horizontal axis).\n\
+\n\
+v\n\
+  Output velocities in ``y`` (vertical axis).\n\
+\n\
+warped_i2\n\
+  i2 as estimated by the optical flow field from i1\n\
+\n\
+");
+
+PyObject* flow(PyObject*, PyObject* args, PyObject* kwds) {
+
+  /* Parses input arguments in a single shot */
+  static const char* const_kwlist[] = {
+    "i1",
+    "i2",
+    "alpha",
+    "ratio",
+    "min_width",
+    "n_outer_fp_iterations",
+    "n_inner_fp_iterations",
+    "n_cg_iterations",
+    0
+  };
+  static char** kwlist = const_cast<char**>(const_kwlist);
+
+  PyBlitzArrayObject* i1 = 0;
+  PyBlitzArrayObject* i2 = 0;
+  double alpha = 0.02;
+  double ratio = 0.75;
+  Py_ssize_t min_width = 30;
+  Py_ssize_t n_outer_fp_iterations = 20;
+  Py_ssize_t n_inner_fp_iterations = 1;
+  Py_ssize_t n_cg_iterations = 50;
+
+  if (!PyArg_ParseTupleAndKeywords(args, kwds, "O&O&|ddnnnn", kwlist,
+        &PyBlitzArray_Converter, &i1,
+        &PyBlitzArray_Converter, &i2,
+        &alpha,
+        &ratio,
+        &min_width,
+        &n_outer_fp_iterations,
+        &n_inner_fp_iterations,
+        &n_cg_iterations
+        ))
+    return 0;
+
+  auto i2_ = make_safe(i2);
+
+  PyBlitzArrayObject* tmp = 0;
+
+  //make sure i1 is convertible to float64
+  tmp = PyBlitzArray_Cast(i1, NPY_FLOAT64);
+  Py_DECREF(i1);
+  i1 = tmp;
+  if (!i1) return 0;
+  auto i1_ = make_safe(i1);
+
+  //make sure i2 is convertible to float64
+  tmp = PyBlitzArray_Cast(i2, NPY_FLOAT64);
+  Py_DECREF(i2);
+  i2 = tmp;
+  if (!i2) return 0;
+  auto i2_ = make_safe(i2);
+
+  //some checks
+  if (i1->ndim != 2 || i1->ndim != 3) {
+    PyErr_SetString(PyExc_TypeError, "method only supports 1D or 2D arrays for input image `i1', but you passed an array with %" PY_FORMAT_SIZE_T "d dimensions", i1->ndim);
+    return 0;
+  }
+
+  if (i1->ndim != i2->ndim) {
+    PyErr_SetString(PyExc_TypeError, "input image arrays must have the same number of dimensions, but image `i1' has %" PY_FORMAT_SIZE_T "d dimensions while image `i2' has %" PY_FORMAT_SIZE_T "d", i1->ndim, i2->ndim);
+    return 0;
+  }
+
+  if (i1->ndim == 2) {
+    if (i1>shape[0] != i2->shape[0] || i2->shape[1] != i2->shape[1]) {
+    PyErr_Format(PyExc_RuntimeError, "shapes of the input images differ: (%" PY_FORMAT_SIZE_T "d, %" PY_FORMAT_SIZE_T "d) != (%" PY_FORMAT_SIZE_T ", %" PY_FORMAT_SIZE_T ")", i1->shape[0], i1->shape[1], i2->shape[0], i2->shape[1]);
+    return 0;
+    }
+  }
+  else { //ndim == 3
+    if (i1->shape[0] != i2->shape[0] || i2->shape[1] != i2->shape[1] ||
+        i2->shape[2] != i2->shape[2]) {
+      PyErr_Format(PyExc_RuntimeError, "shapes of the input images differ: (%" PY_FORMAT_SIZE_T "d, %" PY_FORMAT_SIZE_T "d, %" PY_FORMAT_SIZE_T "d) != (%" PY_FORMAT_SIZE_T ", %" PY_FORMAT_SIZE_T ", %" PY_FORMAT_SIZE_T "d)", i1->shape[0], i1->shape[1], i1->shape[2], i2->shape[0], i2->shape[1], i2->shape[2]);
+      return 0;
+    }
+  }
+
+  return coarse2fine_flow(i1, i2, alpha, ratio, min_width,
+      n_outer_fp_iterations, n_inner_fp_iterations, n_cg_iterations);
+
 }
 
-BOOST_PYTHON_MODULE(_cg_based) {
-  bob::python::setup_python("Bindings to Ce Liu's Optical Flow dense estimator (using Conjugate-Gradient)");
+static PyMethodDef module_methods[] = {
+    {
+      s_flow_str,
+      (PyCFunction)flow,
+      METH_VARARGS|METH_KEYWORDS,
+      s_flow_doc
+    },
+    {0}  /* Sentinel */
+};
 
-  boost::python::def("flow", coarse2fine_flow, cg::coarse2fine_flow_overloads((
-          boost::python::arg("i1"),
-          boost::python::arg("i2"),
-          boost::python::arg("alpha")=0.02,
-          boost::python::arg("ratio")=0.75,
-          boost::python::arg("min_width")=30,
-          boost::python::arg("n_outer_fp_iterations")=20,
-          boost::python::arg("n_inner_fp_iterations")=1,
-          boost::python::arg("n_cg_iterations")=50),
-        "Computes dense optical flow field in a coarse to fine manner using conjugate gradient (CG)\n"
-        "\n"
-        "This method computes the dense optical flow field using a coarse-to-fine approach. C++ code running under this call is extracted from **the old version (pre Aug 1, 2011)** of `Ce Liu's homepage <http://people.csail.mit.edu/celiu/OpticalFlow/>`_ and should give the exact same output as the Matlab equivalent.\n\n"
-        "N.B.: This variant does not use the Successive Over-Relaxation (SOR) that was implemented on August 1st., 2011 by C. Liu, but the old version based on Conjugate-Gradient (CG).\n"
-        "\n"
-        "Keyword Parameters:\n"
-        "\n"
-        "i1\n"
-        "  First input frame (grayscale/double image)\n"
-        "i2\n"
-        "  Second input frame (same dimension and type of the first frame)\n"
-        "alpha\n"
-        "  [optional] Regularization weight\n"
-        "ratio\n"
-        "  [optional] Downsample ratio\n"
-        "min_width\n"
-        "  [optional] Width of the coarsest level\n"
-        "n_outer_fp_iterations\n"
-        "  [optional] The number of outer fixed point iterations\n"
-        "n_inner_fp_iterations\n"
-        "  [optional] The number of inner fixed point iterations\n"
-        "n_cg_iterations\n"
-        "  [optional] The number of conjugate-gradient (CG) iterations\n"
-        "\n"
-        "Returns a tuple containing three 2D double arrays with the same dimensions as the input images:\n"
-        "\n"
-        "u\n"
-        "  Output velocities in ``x`` (horizontal axis).\n"
-        "v\n"
-        "  Output velocities in ``y`` (vertical axis).\n"
-        "warped_i2\n"
-        "  i2 as estimated by the optical flow field from i1\n"
-        )
-        );
+PyDoc_STRVAR(module_docstr, "Ce Liu's Optical Flow implementations");
+
+#if PY_VERSION_HEX >= 0x03000000
+static PyModuleDef module_definition = {
+  PyModuleDef_HEAD_INIT,
+  XBOB_EXT_MODULE_NAME,
+  module_docstr,
+  -1,
+  module_methods,
+  0, 0, 0, 0
+};
+#endif
+
+static PyObject* create_module (void) {
+
+# if PY_VERSION_HEX >= 0x03000000
+  PyObject* m = PyModule_Create(&module_definition);
+# else
+  PyObject* m = Py_InitModule3(XBOB_EXT_MODULE_NAME, module_methods, module_docstr);
+# endif
+  if (!m) return 0;
+  auto m_ = make_safe(m); ///< protects against early returns
+
+  if (PyModule_AddStringConstant(m, "__version__", XBOB_EXT_MODULE_VERSION) < 0)
+    return 0;
+
+  /* imports xbob.blitz C-API + dependencies */
+  if (import_xbob_blitz() < 0) return 0;
+
+  Py_INCREF(m);
+  return m;
+
+}
+
+PyMODINIT_FUNC XBOB_EXT_ENTRY_NAME (void) {
+# if PY_VERSION_HEX >= 0x03000000
+  return
+# endif
+    create_module();
 }
