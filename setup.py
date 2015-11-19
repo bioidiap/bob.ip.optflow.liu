1f5c3f1672a6c19381cabd8e4a05f091a4ec45ae
Pavel Korshunov <pavel.korshunov@idiap.ch>
2015-11-19 15:38:34 +0100

    pkg_resources is replaces with pkgutil

--- setup.py
+++ setup.py
@@ -31,11 +31,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
 
-    namespace_packages=[
-      "bob",
-      "bob.ip",
-      "bob.ip.optflow",
-    ],
+
 
     setup_requires = build_requires,
     install_requires = build_requires,
